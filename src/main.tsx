170f0bc42ec805cd84e6ebb58e8f63b49565cbeb
BetterhavemyMonet <xomonetox168@outlook.com>
2026-04-18 18:58:43 -0500

    Recovered Monet Arcade from live build

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import __buffer_polyfill from "/node_modules/.vite/deps/vite-plugin-node-polyfills_shims_buffer.js?v=1a8f1b92"
+globalThis.Buffer = globalThis.Buffer || __buffer_polyfill
+import __global_polyfill from "/node_modules/.vite/deps/vite-plugin-node-polyfills_shims_global.js?v=1a8f1b92"
+globalThis.global = globalThis.global || __global_polyfill
+import __process_polyfill from "/node_modules/.vite/deps/vite-plugin-node-polyfills_shims_process.js?v=1a8f1b92"
+globalThis.process = globalThis.process || __process_polyfill
+
+"use strict";
+import __vite__cjsImport3_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=1a8f1b92"; const jsxDEV = __vite__cjsImport3_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport4_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=1a8f1b92"; const createRoot = __vite__cjsImport4_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+import "/src/index.css";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, { "data-replit-metadata": "artifacts/monet-arcade/src/main.tsx:5:52", "data-component-name": "App" }, void 0, false, {
+  fileName: "/home/runner/workspace/artifacts/monet-arcade/src/main.tsx",
+  lineNumber: 5,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFJb0Q7QUFKcEQsU0FBU0Esa0JBQWtCO0FBQzNCLE9BQU9DLFNBQVM7QUFDaEIsT0FBTztBQUVQRCxXQUFXRSxTQUFTQyxlQUFlLE1BQU0sQ0FBRSxFQUFFQyxPQUFPLHVCQUFDLE9BQUUsb0dBQUg7QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUFJLENBQUciLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQXBwIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSBcInJlYWN0LWRvbS9jbGllbnRcIjtcbmltcG9ydCBBcHAgZnJvbSBcIi4vQXBwXCI7XG5pbXBvcnQgXCIuL2luZGV4LmNzc1wiO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSEpLnJlbmRlcig8QXBwIC8+KTtcbiJdLCJmaWxlIjoiL2hvbWUvcnVubmVyL3dvcmtzcGFjZS9hcnRpZmFjdHMvbW9uZXQtYXJjYWRlL3NyYy9tYWluLnRzeCJ9
